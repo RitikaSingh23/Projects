5bda7c1bd6e35592d2498f8a94280d541c33689d
RitikaSingh23 <ritika2302singh@gmail.com>
2025-03-03 20:12:13 +0530

    add

--- MovieTicketBooking/signup.js
+++ MovieTicketBooking/signup.js
@@ -28,19 +28,95 @@ var typed = new Typed("#typed-text", {
 });
 
 // Sign-Up Form Validation
-document.getElementById("signupForm").addEventListener("submit", function (event) {
-    event.preventDefault();
+let submit=()=>{
+    let inpname=document.querySelector("#name").value;
+    let inpnum=document.querySelector("#number").value;
+    let inpemail=document.querySelector("#email").value;
+    let inppass=document.querySelector("#password").value;
+    let inpcpass=document.querySelector("#cpassword").value;
 
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("confirmPassword").value;
+    let errname=document.querySelector("#errname")
+    let errnum=document.querySelector("#errnumber")
+    let erremail=document.querySelector("#erremail")
+    let errpass=document.querySelector("#errpass")
+    let errcpass=document.querySelector("#errcpass")
 
-    if (password !== confirmPassword) {
-        alert("Passwords do not match!");
-        return;
+    if(inpname=="")
+    {
+        errname.innerHTML="*Please fill the name field"
+        errname.style.color="red"
+        document.querySelector("#name").style.border = "2px solid red"
+        document.querySelector("#name")
+        return false;
     }
 
-    alert("Sign-up successful! Redirecting to login...");
-    window.location.href = "index.html";
-});
+    else if(isNaN(inpnum))
+    {
+        errnum.innerHTML="*Please enter number only"
+        errnum.style.color="red"
+        document.querySelector("#name").style.border = ""
+        errname.innerHTML=""
+        document.querySelector("#number").style.border = "2px solid red"
+        document.querySelector("#number").focus()
+        return false
+    }
+
+    else if(inpnum.length!=10)
+    {
+        errnum.innerHTML="*please enter 10 digits only"
+        errnum.style.color="red"
+        document.querySelector("#number").style.border = "2px solid red"
+        document.querySelector("#number").focus()
+        errname.innerHTML=""
+        return false;
+    }
+
+    else if(!(inpemail.includes('@') && inpemail.includes('.com')))
+    {
+        erremail.innerHTML="*Please enter valid email"
+        erremail.style.color="red"
+        document.querySelector("#email").style.border = "2px solid red"
+        document.querySelector("#email").focus()
+        errname.innerHTML=""
+        errnum.innerHTML=""
+        errnum.innerHTML=""
+        return false;
+    }
+
+    else if(inppass!=inpcpass)
+    {
+        errcpass.innerHTML="*Please enter same password"
+        errcpass.style.color="red"
+        document.querySelector("#cpassword").style.border = "2px solid red"
+        document.querySelector("#cpassword").value=""
+        document.querySelector("#cpassword").focus()
+        errname.innerHTML=""
+        errnum.innerHTML=""
+        errnum.innerHTML=""
+        return false;
+    }
+
+    else if(!(inppass.match(/[1234567890]/) 
+        && inppass.match(/[!@#%&]/)
+        &&inppass.match(/[ a-z]/)
+        && inppass.match(/[A-Z]/)))
+    {
+        errpass.innerHTML="*please enter strong password"
+        errpass.style.color="red"
+        errname.innerHTML=""
+        return false
+    }
+
+        localStorage.setItem("Name",inpname)
+        localStorage.setItem("Number",inpnum)
+        localStorage.setItem("E-mail",inpemail)
+        localStorage.setItem("Password",inppass)
+        localStorage.setItem("Cpassword",inpcpass)
+
+
+        alert("Sign-up successful! Redirecting to login...")
+        location.href="Login.html";
+
+
+        return false
+}
