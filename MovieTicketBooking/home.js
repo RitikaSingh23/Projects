1edb38a09d5af4ad75e602207d81765bd650d3e7
RitikaSingh23 <ritika2302singh@gmail.com>
2025-03-09 22:59:17 +0530

    features

--- MovieTicketBooking/home.js
+++ MovieTicketBooking/home.js
@@ -17,8 +17,12 @@ var typed = new Typed("#typed", {
 
 //swipper .js Effect
 var swiper = new Swiper(".mySwiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+  slidesPerView: 4,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
+
